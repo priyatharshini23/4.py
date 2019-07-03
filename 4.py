e855c7c27691d3139e6e0fbed6d9567131b75b57
priyatharshini23 <52314722+priyatharshini23@users.noreply.github.com>
2019-07-03 14:17:33 +0530

    Update and rename README.md to 4.py

---
+++ 4.py
@@ -0,0 +1,6 @@
+# 4.py
+Z=input("Enter a input ")
+if(Z=='A' or Z=='a' or Z=='E' or Z=='e' or Z=='I' or Z=='O' or Z=='o' or Z=='U' or Z=='u'):
+     print(Z,"is a vowel")
+else:
+     print(Z,"is a consonant")
